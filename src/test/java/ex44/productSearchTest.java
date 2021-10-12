d0e90ae4cfb3b8f05c24e080c6618f92c939f354
MohTaher108 <hamodetaher@gmail.com>
2021-10-11 23:51:31 -0400

    fixed comments

--- src/test/java/ex44/productSearchTest.java
+++ src/test/java/ex44/productSearchTest.java
@@ -8,8 +8,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class productSearchTest {
     @Test
     void iterateThroughFile() throws Exception {
+        // Create a JSONArray,from our jsonArray creating function
         productSearch test = new productSearch();
         JSONArray JArray = test.makejsonArray();
+        // Test that the iterateThroughFile function returns the correct thing
         assertEquals(true, test.iterateThroughFile(JArray, "Thing"));
         assertEquals(false, test.iterateThroughFile(JArray, "nothing"));
     }
